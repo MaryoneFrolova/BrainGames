ba04c7306948908b5e6082d129d335b3b302044e
MaryoneFrolova <35322384+MaryoneFrolova@users.noreply.github.com>
2018-01-23 22:35:58 +1000

    step 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const nameUser = () => {
+const nameUser = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
 };
+
+export default nameUser;
