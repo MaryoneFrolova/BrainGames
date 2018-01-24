e8cd251e4c6f4128ba620e8f628b9cf8682ff27f
MaryoneFrolova <35322384+MaryoneFrolova@users.noreply.github.com>
2018-01-24 11:47:53 +1000

    step 4

--- src/index.js
+++ src/index.js
@@ -6,22 +6,22 @@ export const nameUser = () => {
   return name;
 };
 
-const isEven = (number) => number % 2 === 0 ? 'yes' : 'no';
+const isEven = number => (number % 2 === 0) ? 'yes' : 'no';
 
 export const game = () => {
   console.log('Welcome to the Brain Games!');
-  console.log('Answer \"yes\" if number even otherwise answer \"no\".\n')
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
   const name = nameUser();
   const countQuestion = 3;
   for (let i = 1; i <= countQuestion; i++) {
-    let currentNumber = Math.floor(Math.random() * 100 + 1);
+    const currentNumber = Math.floor(Math.random() * 100) + 1;
     console.log(`Question: ${currentNumber}`);
     const answerQuestion = readlineSync.question('Your answer: ');
     if (answerQuestion !== isEven(currentNumber)) {
       console.log(`'${answerQuestion}' is wrong answer ;(. Correct answer was '${isEven(currentNumber)}'.`);
       console.log(`Let's try again, ${name}!`);
       break;
-    } 
+      } 
     else {
       console.log('Correct!');
     }
