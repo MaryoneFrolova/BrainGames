230e88e746ecc38bb705beb3c1376a423aad2beb
MaryoneFrolova <35322384+MaryoneFrolova@users.noreply.github.com>
2018-01-24 18:50:10 +1000

    step 5

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { game } from '..';
 
-const task = 'What is the result of the expression?';
+const task = 'What is the result of the expression?\n';
 const gameName = 'calc';
 game(gameName, task);
