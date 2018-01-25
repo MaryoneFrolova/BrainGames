b1f8e1dae4aad2a79242df2311547c705f4f7c0a
MaryoneFrolova <35322384+MaryoneFrolova@users.noreply.github.com>
2018-01-25 17:22:59 +1000

    step 7
    game balance number

---
+++ src/games/game-balance.js
@@ -0,0 +1,44 @@
+// game GCD: Find the greatest common divisor of given numbers.
+import { cons } from 'hexlet-pairs';
+import { game } from '..';
+
+const ruleGame = 'Balance the given number. \n';
+
+const balance = (number) => {
+  let currentNumber = number;
+  if (number / 10 === 0) {
+    return number;
+  }
+  let result = 0;
+  let count = 1;
+  let sum = 0;
+  while (parseInt(currentNumber / 10, 10) !== 0) {
+    sum += currentNumber % 10;
+    count += 1; // количество цифр числа
+    currentNumber = parseInt(currentNumber / 10, 10);
+  }
+  sum += currentNumber % 10; // сумма всех цифр числа
+  const avarage = parseInt(sum / count, 10); // среднее приходящееся на каждое число
+  let def = sum % count; // количество единиц, которые нужно добавить с конца
+  for (let i = 0; i < count; i += 1) {
+    result += avarage * (10 ** i);
+    if (def > 0) {
+      result += 10 ** i;
+      def -= 1;
+    }
+  }
+  return result;
+};
+
+export const taskBalance = () => {
+  const number = Math.floor(Math.random() * 1000) + 1;
+  const stringQuestion = `${number}`;
+  const rightAnswer = balance(number);
+  return cons(stringQuestion, rightAnswer);
+};
+
+const gameBalance = () => {
+  game(taskBalance, ruleGame);
+};
+
+export default gameBalance;
